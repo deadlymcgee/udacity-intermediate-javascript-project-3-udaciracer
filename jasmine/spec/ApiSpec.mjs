e8ed4577f902ae21585c70f8a866bf97417d5df0
npwaters <40352015+npwaters@users.noreply.github.com>
2021-06-28 16:51:58 +1000

    feat: Implement mocked API calls

--- jasmine/spec/ApiSpec.mjs
+++ jasmine/spec/ApiSpec.mjs
@@ -1,8 +1,34 @@
 import Api from "../../src/client/assets/javascript/api";
 
+const JasmineHelpers = (function() {
+  const successful = function() {
+    return new Promise(resolve => {
+      resolve("Successful!");
+    })
+  }
+  const failed = function() {
+    return new Promise((resolve, reject) => reject("Failed!"))
+      .catch(err => console.log(err))
+  }
+  return {
+    success: successful,
+    failed: failed
+  }
+})();
+
 describe("Index", function() {
   it("should be able handle a successful API call", async function() {
+    spyOn(Api, 'getTracks').and.callFake(async function() {
+      return await JasmineHelpers.success();
+    })
     const result = await Api.getTracks();
     expect(result).not.toBe(undefined);
+  })
+  it("should be able handle an unsuccessful API call", async function() {
+    spyOn(Api, 'getTracks').and.callFake(async function() {
+      return await JasmineHelpers.failed();
+    })
+    const result = await Api.getTracks();
+    expect(result).toBe(undefined);
   })
 });
